2feebfb2d01517d946d42adbaefc9f2d49a833c8
谢翔 <xiex.pagoda.com.cn>
2020-04-14 03:10:47 +0800

    add check tag new commit id equal prod new commit id and add retry 3

--- add_tag.sh
+++ add_tag.sh
@@ -4,17 +4,23 @@
 #$2 tag_version
 #$3 project_name
 
+project_num=0
+error_project=()
+
 init_params() {
     x=0
     d=0
     hvn=5
     mark=''
     empty=''
+    is_run=false
     tag_arr=()
-    valid_version=false
     reserved_arr=()
     is_branch=false
+    jump_check=false
     is_checkout=true
+    valid_version=false
+    check_num=3
     max_tag_version_number=0
 }
 
@@ -132,7 +138,10 @@ function git_add_tag(){
     if [[ -d ${project_name} ]];then
 
         echo -e "\033[33m in project <<<${project_name}>>> \033[0m"
-        cd ${project_name}
+
+        if [[ `pwd | grep "${project_name}"` == "" ]]; then
+            cd ${project_name}
+        fi
 
         git tag | xargs git tag -d
         git fetch
@@ -174,13 +183,83 @@ function git_add_tag(){
 
             git push --tags -f
 
-            echo -e "\033[32m push remote tag: <<<${tag_version}>>> success! \033[0m"
+            check_tag_equal_prod_commit ${branch_name} ${tag_version} ${project_name}
+
         fi
         init_params
         cd ../
 
     else
         echo -e "\033[31m no project!!!<<<${project_name}>>> \033[0m"
+    fi
+}
+
+function check_tag_equal_prod_commit() {
+
+
+
+        if [[ -d ${3} ]]; then
+            cd ${3}
+            is_run=true
+        else
+            if [[ `pwd | grep "${3}"` != "" ]]; then
+                is_run=true
+            fi
+        fi
+if [[ ${is_run} == true ]]; then
+
+    prod_commit_id=`git log --pretty="%h" -1`
+
+    if [[ ${check_num} < 1 ]]; then
+        jump_check=true
+        error_project[$project_num]="${3}"
+     
+    else        
+
+        if [[ `git tag | grep ${2}` != ${empty} ]]; then
+
+            tag_commit_id=`git show ${2} --pretty="%h" -1`
+            if [[ ${prod_commit_id} == $tag_commit_id ]]; then
+                jump_check=true
+                echo -e "\033[32m push remote tag: <<<${2} ${3}>>> success! \033[0m"
+            else
+               #git_add_tag 
+               let check_num--
+
+               check_tag_equal_prod_commit $1 $2 $3
+           fi
+        else
+            #git_add_tag 
+            let check_num--
+
+            check_tag_equal_prod_commit $1 $2 $3
+        fi
+    fi
+fi
+if [[ ${jump_check} == true ]]; then
+        let project_num++
+        init_params
+        cd ../    
+    #statements
+fi
+
+}
+
+function run() {
+
+    if [[ ${3} != ${empty} ]];then
+
+        check_tag_equal_prod_commit $1 $2 $3
+#    git_add_tag $1 $2 $3
+
+    else
+
+        for file in $(ls ./)
+        do
+        check_tag_equal_prod_commit $1 $2 ${file}
+#            git_add_tag $1 $2 ${file}
+        done
+
     fi
 }
 
@@ -191,17 +270,6 @@ if [[ ${2} == ${empty} ]];then
     return
 fi
 
-if [[ ${3} != ${empty} ]];then
+run $1 $2 $3
 
-    git_add_tag $1 $2 $3
-
-else
-
-    for file in $(ls ./)
-    do
-        git_add_tag $1 $2 ${file}
-    done
-
-fi
-
-
+echo "最终失败的："${error_project[*]}
